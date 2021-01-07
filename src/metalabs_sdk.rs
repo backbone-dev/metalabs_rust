93fe383747ea5bcef0d7f80ad52f454a92f69ba6
Imag <44352064+Imag@users.noreply.github.com>
2021-01-07 05:51:00 -0800

    Add files via upload

---
+++ src/metalabs_sdk.rs
@@ -0,0 +1,58 @@
+use reqwest::blocking;
+use std::collections::HashMap;
+
+pub fn build_api_url(key: &str) -> String {
+    return format!("https://api.metalabs.io/v2/licenses/{}", key);
+}
+
+pub struct Client {
+    pub auth_key: String,
+}
+
+impl Client {
+    pub fn new(key: &str) -> Client {
+        Client {
+            auth_key: String::from(key),
+        }
+    }
+
+    fn get_key(&self, key: &str) -> String {
+        let response = blocking::Client::new()
+            .get(&build_api_url(key))
+            .header("Authorization", format!("Basic {}", self.auth_key))
+            .send();
+
+        let response = match response {
+            Ok(res) => res,
+            Err(error) => panic!("Error Occured: {:#?}", error),
+        };
+
+        let data = match response.text() {
+            Ok(content) => content,
+            Err(error) => panic!("Error Occured: {:#?}", error),
+        };
+
+        return data;
+    }
+
+    fn update_key(&self, key: &str, meta: HashMap<&str, &str>) -> String {
+        let response = blocking::Client::new()
+            .patch(&build_api_url(key))
+            .json(format!("{{'metadata': {:#?}}}", &meta))
+            .header("Authorization", format!("Basic {}", self.auth_key))
+            .header("Content-Type", "application/json")
+            .send();
+
+        let response = match response {
+            Ok(res) => res,
+            Err(error) => panic!("Error Occured: {:#?}", error),
+        };
+
+        let data = match response.text() {
+            Ok(content) => content,
+            Err(error) => panic!("Error Occured: {:#?}", error),
+        };
+
+        return data;
+    }
+}
